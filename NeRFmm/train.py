ca9bd6e35289a1be13c36cb2b406541914c71594
Sebastian Berndt <email+github@sebastian-berndt.com>
2021-08-09 10:56:03 +0200

    added nerf--

---
+++ NeRFmm/train.py
@@ -0,0 +1,84 @@
+def model_render_image(c2w, rays_cam, t_vals, ray_params, H, W, fxfy, nerf_model,
+                       perturb_t, sigma_noise_std):
+    """
+    :param c2w:         (4, 4)                  pose to transform ray direction from cam to world.
+    :param rays_cam:    (someH, someW, 3)       ray directions in camera coordinate, can be random selected
+                                                rows and cols, or some full rows, or an entire image.
+    :param t_vals:      (N_samples)             sample depth along a ray.
+    :param perturb_t:   True/False              perturb t values.
+    :param sigma_noise_std: float               add noise to raw density predictions (sigma).
+    :return:            (someH, someW, 3)       volume rendered images for the input rays.
+    """
+    # KEY 2: sample the 3D volume using estimated poses and intrinsics online.
+    # (H, W, N_sample, 3), (H, W, 3), (H, W, N_sam)
+    sample_pos, _, ray_dir_world, t_vals_noisy = volume_sampling_ndc(c2w, rays_cam, t_vals, ray_params.NEAR,
+                                                                     ray_params.FAR, H, W, fxfy, perturb_t)
+
+    # encode position: (H, W, N_sample, (2L+1)*C = 63)
+    pos_enc = encode_position(sample_pos, levels=ray_params.POS_ENC_FREQ, inc_input=True)
+
+    # encode direction: (H, W, N_sample, (2L+1)*C = 27)
+    ray_dir_world = F.normalize(ray_dir_world, p=2, dim=2)  # (H, W, 3)
+    dir_enc = encode_position(ray_dir_world, levels=ray_params.DIR_ENC_FREQ, inc_input=True)  # (H, W, 27)
+    dir_enc = dir_enc.unsqueeze(2).expand(-1, -1, ray_params.N_SAMPLE, -1)  # (H, W, N_sample, 27)
+
+    # inference rgb and density using position and direction encoding.
+    rgb_density = nerf_model(pos_enc, dir_enc)  # (H, W, N_sample, 4)
+
+    render_result = volume_rendering(rgb_density, t_vals_noisy, sigma_noise_std, rgb_act_fn=torch.sigmoid)
+    rgb_rendered = render_result['rgb']  # (H, W, 3)
+    depth_map = render_result['depth_map']  # (H, W)
+
+    result = {
+        'rgb': rgb_rendered,  # (H, W, 3)
+        'depth_map': depth_map,  # (H, W)
+    }
+
+    return result
+
+
+def train_one_epoch(imgs, H, W, ray_params, opt_nerf, opt_focal,
+                    opt_pose, nerf_model, focal_net, pose_param_net):
+    nerf_model.train()
+    focal_net.train()
+    pose_param_net.train()
+
+    t_vals = torch.linspace(ray_params.NEAR, ray_params.FAR, ray_params.N_SAMPLE, device='cuda')  # (N_sample,) sample position
+    L2_loss_epoch = []
+
+    # shuffle the training imgs
+    ids = np.arange(N_IMGS)
+    np.random.shuffle(ids)
+
+    for i in ids:
+        fxfy = focal_net()
+
+        # KEY 1: compute ray directions using estimated intrinsics online.
+        ray_dir_cam = comp_ray_dir_cam_fxfy(H, W, fxfy[0], fxfy[1])
+        img = imgs[i].to('cuda')  # (H, W, 4)
+        c2w = pose_param_net(i)  # (4, 4)
+
+        # sample 32x32 pixel on an image and their rays for training.
+        r_id = torch.randperm(H, device='cuda')[:32]  # (N_select_rows)
+        c_id = torch.randperm(W, device='cuda')[:32]  # (N_select_cols)
+        ray_selected_cam = ray_dir_cam[r_id][:, c_id]  # (N_select_rows, N_select_cols, 3)
+        img_selected = img[r_id][:, c_id]  # (N_select_rows, N_select_cols, 3)
+
+        # render an image using selected rays, pose, sample intervals, and the network
+        render_result = model_render_image(c2w, ray_selected_cam, t_vals, ray_params,
+                                           H, W, fxfy, nerf_model, perturb_t=True, sigma_noise_std=0.0)
+        rgb_rendered = render_result['rgb']  # (N_select_rows, N_select_cols, 3)
+        L2_loss = F.mse_loss(rgb_rendered, img_selected)  # loss for one image
+
+        L2_loss.backward()
+        opt_nerf.step()
+        opt_focal.step()
+        opt_pose.step()
+        opt_nerf.zero_grad()
+        opt_focal.zero_grad()
+        opt_pose.zero_grad()
+
+        L2_loss_epoch.append(L2_loss)
+
+    L2_loss_epoch_mean = torch.stack(L2_loss_epoch).mean().item()
+    return L2_loss_epoch_mean
